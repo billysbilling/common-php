77e117f21cc8b13db32d083f1107d2732ba6a84c
Lars Schou <lars@billy.dk>
2021-05-20 13:09:00 +0200

    Simplify load of options

--- src/Aws/ClientFactory.php
+++ src/Aws/ClientFactory.php
@@ -3,6 +3,7 @@
 namespace Common\Aws;
 
 use Aws\Credentials\Credentials;
+use Aws\EventBridge\EventBridgeClient;
 use Aws\S3\S3Client;
 
 class ClientFactory
@@ -12,12 +13,7 @@ class ClientFactory
      */
     public static function getS3Client(): S3Client
     {
-        return new S3Client([
-            'credentials' => self::getCredentials(),
-            'region' => \getenv('S3_REGION') ?: \getenv('AWS_REGION'),
-            'signature_version' => 'v4',
-            'version' => '2006-03-01'
-        ]);
+        return new S3Client(array_merge(['signature_version' => 'v4', self::defaultOptions()]));
     }
 
     /**
@@ -25,17 +21,27 @@ class ClientFactory
      */
     public static function getSNSClient(): SnsClient
     {
-        return new SnsClient([
+        return new SnsClient(self::defaultOptions());
+    }
+
+    public static function getEventBridgeClient(): EventBridgeClient
+    {
+        return new EventBridgeClient(self::defaultOptions());
+    }
+
+    private static function defaultOptions(): array
+    {
+        return [
             'credentials' => self::getCredentials(),
-            'region' => \getenv('SNS_REGION') ?: \getenv('AWS_REGION'),
+            'region' => \getenv('AWS_REGION'),
             'version' => '2010-03-31'
-        ]);
+        ];
     }
 
     /**
      * @return Credentials
      */
-    public static function getCredentials(): Credentials
+    private static function getCredentials(): Credentials
     {
         return new Credentials(\getenv('AWS_API_KEY'), \getenv('AWS_SECRET_KEY'));
     }
