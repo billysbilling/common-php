85f8c8f45b8b51ea963312c6f9d856b1c284cd37
Lars Schou <lars@billy.dk>
2021-05-25 13:48:16 +0200

    Add fallback for region

--- src/Aws/ClientFactory.php
+++ src/Aws/ClientFactory.php
@@ -3,7 +3,6 @@
 namespace Common\Aws;
 
 use Aws\Credentials\CredentialProvider;
-use Aws\Credentials\Credentials;
 use Aws\S3\S3Client;
 
 /**
@@ -14,7 +13,7 @@ class ClientFactory
 {
     public static function getS3Client(): S3Client
     {
-        return new S3Client(array_merge(['signature_version' => 'v4', self::defaultOptions()]));
+        return new S3Client(array_merge(self::defaultOptions(), ['signature_version' => 'v4']));
     }
 
     public static function getSNSClient(): SnsClient
@@ -31,7 +30,7 @@ class ClientFactory
     {
         return [
             'credentials' => CredentialProvider::defaultProvider(),
-            'region' => \getenv('AWS_REGION'),
+            'region' => \getenv('AWS_REGION') ?: 'eu-north-1',
             'version' => '2010-03-31'
         ];
     }
