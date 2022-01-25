a17aeb5b752b14c612c37dca0aca1ae5ae1cd4ed
Jacek Zlowocki <jacek.zlowocki@gmail.com>
2022-01-25 11:54:07 +0100

    HAM-900: support mockHandler in AWS clients (#3)

--- src/Aws/ClientFactory.php
+++ src/Aws/ClientFactory.php
@@ -4,6 +4,7 @@ namespace Common\Aws;
 
 use Aws\Credentials\CredentialProvider;
 use Aws\S3\S3Client;
+use Aws\MockHandler;
 
 /**
  * Class ClientFactory
@@ -11,33 +12,34 @@ use Aws\S3\S3Client;
  */
 class ClientFactory
 {
-    public static function getS3Client(): S3Client
+    public static function getS3Client(MockHandler $mockHandler = null): S3Client
     {
-        return new S3Client(array_merge(self::defaultOptions(), [
+        return new S3Client(array_merge(self::defaultOptions($mockHandler), [
             'signature_version' => 'v4',
             'version' => '2006-03-01'
         ]));
     }
 
-    public static function getSNSClient(): SnsClient
+    public static function getSNSClient(MockHandler $mockHandler = null): SnsClient
     {
-        return new SnsClient(array_merge(self::defaultOptions(), [
+        return new SnsClient(array_merge(self::defaultOptions($mockHandler), [
             'version' => '2010-03-31'
         ]));
     }
 
-    public static function getEventBridgeClient(): EventBridgeClient
+    public static function getEventBridgeClient(MockHandler $mockHandler = null): EventBridgeClient
     {
-        return new EventBridgeClient(array_merge(self::defaultOptions(), [
+        return new EventBridgeClient(array_merge(self::defaultOptions($mockHandler), [
             'version' => '2015-10-07'
         ]));
     }
 
-    private static function defaultOptions(): array
+    private static function defaultOptions(MockHandler $mockHandler = null): array
     {
         return [
             'credentials' => CredentialProvider::defaultProvider(),
             'region' => \getenv('AWS_REGION') ?: 'eu-north-1',
+            'handler' => $mockHandler,
         ];
     }
 }
