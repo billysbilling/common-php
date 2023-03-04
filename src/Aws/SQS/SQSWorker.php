6f6b36cf62946b0a7eb20ab34b398570967e23e1
Lars Schou <lars@billy.dk>
2023-03-04 11:51:46 +0100

    Remove those stupid colors as it gives no value

--- src/Aws/SQS/SQSWorker.php
+++ src/Aws/SQS/SQSWorker.php
@@ -9,11 +9,6 @@ class SQSWorker extends SQSBase
     public int $waitTimeSeconds = 20;
     public int $maxNumberOfMessages = 1;
     public int $visibilityTimeout = 360;
-
-    private const LEVEL_SUCCESS = 'success';
-    private const LEVEL_WARNING = 'warning';
-    private const LEVEL_DANGER = 'danger';
-
     public function listen(string $queueName, callable $workerProcess, callable $errorHandlerCallback = null): void
     {
         $this->queueUrl = $this->getQueueUrl($queueName);
@@ -27,15 +22,15 @@ class SQSWorker extends SQSBase
                 $this->getMessages(function (array $messages) use ($workerProcess) {
                     foreach ($messages as $value) {
                         $job = new SQSJob($value);
-                        $this->out('Processing ' . $job->getMessageId(), self::LEVEL_WARNING);
+                        $this->log('Processing ' . $job->getMessageId(), self::LEVEL_WARNING);
                         $exitCode = $workerProcess($job);
 
                         if ($exitCode === 0 || is_null($exitCode)) {
                             $this->ackMessage($value);
-                            $this->out('Processed ' . $job->getMessageId(), self::LEVEL_SUCCESS);
+                            $this->log('Processed ' . $job->getMessageId(), self::LEVEL_SUCCESS);
                         } else {
                             $this->nackMessage($value);
-                            $this->out('Failed ' . $job->getMessageId() . '. ', self::LEVEL_DANGER);
+                            $this->log('Failed ' . $job->getMessageId() . '. ', self::LEVEL_DANGER);
                         }
                     }
 
@@ -99,32 +94,16 @@ class SQSWorker extends SQSBase
 
     private function printQueueStarted(): void
     {
-        $this->out('*****************************************************************');
-        $this->out('**** Worker started at ' . date('Y-m-d H:i:s'));
-        $this->out('**** ' . $this->queueUrl);
-        $this->out('*****************************************************************');
+        $this->log('**** Worker started on queue: ' . $this->queueUrl);
     }
 
     private function printQueueEnded(): void
     {
-        $this->out('*****************************************************************');
-        $this->out('**** Worker finished at ' . date('Y-m-d H:i:s'));
-        $this->out('*****************************************************************');
+        $this->log('**** Worker finished on queue: ' . $this->queueUrl);
     }
 
-    private function out($message, ?string $level = null): void
+    private function log($message): void
     {
-        echo PHP_EOL . $this->getCLIColor($level) . $message . "\e[0m";
+        echo PHP_EOL . $message;
     }
-
-    private function getCLIColor(string $level = null): string
-    {
-        return match ($level) {
-            self::LEVEL_SUCCESS => "\e[0;32m",
-            self::LEVEL_WARNING => "\e[0;33m",
-            self::LEVEL_DANGER => "\e[0;31m",
-            default => "\e[0m",
-        };
-    }
-
 }
